aca91d85c334fa5efe57a6e7ae9240983e85b235
csh9411 <csh9411@naver.com>
2021-03-08 02:22:35 +0900

    3.2까지 완료

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(localsMiddlewar);
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy",
+    "script-src 'self' https://archive.org"
+  );
+  return next();
+});
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- middlewares.js
+++ middlewares.js
@@ -3,5 +3,9 @@ import routes from "./routes";
 export const localsMiddlewar = (req, res, next) => {
   res.locals.siteName = "GitTube";
   res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1,
+  };
   next();
 };

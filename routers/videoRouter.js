5aa8c8565bb85733387388a125420208ea3082a2
csh9411 <csh9411@naver.com>
2021-03-09 23:33:00 +0900

    mongodb 완료

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,8 +3,9 @@ import {
   getUpload,
   postUpload,
   deleteVideo,
-  editVideo,
   videoDetail,
+  getEditVideo,
+  postEditVideo,
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 import routes from "../routes";
@@ -14,8 +15,14 @@ const videoRouter = express.Router();
 // videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;

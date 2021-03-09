286d1e5fd658e466b2c9bae485d564f195b51d0b
csh9411 <csh9411@naver.com>
2021-03-09 14:00:20 +0900

    3.8까지 완료

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -6,13 +6,14 @@ import {
   editVideo,
   videoDetail,
 } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 import routes from "../routes";
 
 const videoRouter = express.Router();
 
 // videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
